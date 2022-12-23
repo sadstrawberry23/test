759d766745d9ffa4e64ce09c30e790c62a3e1769
Baidibek <sadstrawberry23@gmail.com>
2022-12-23 18:33:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = +prompt("На соклько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = +prompt("На соклько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
